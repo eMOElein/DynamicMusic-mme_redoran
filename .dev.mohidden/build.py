50c9502ffa2061d795064e3959e4bbf78d088d48
eMOElein <aka_moe@gmx.de>
2024-08-08 13:45:00 +0200

    initial

---
+++ .dev.mohidden/build.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+import time
+
+buildPath = ".build.mohidden"
+projectPath = ".build.mohidden/project"
+buildTime = time.time()
+buildName = "DynamicMusic-MME_Redoran-" +str(int(buildTime))
+
+os.chdir("..")
+
+if os.path.isdir(buildPath):
+  shutil.rmtree(buildPath)
+
+os.makedirs(buildPath)
+os.makedirs(projectPath)
+
+shutil.copytree("scripts", projectPath +"/scripts")
+shutil.make_archive(buildPath +"/" +buildName, 'zip', projectPath)
